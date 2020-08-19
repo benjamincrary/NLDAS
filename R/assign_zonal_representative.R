95d5f941be1fff8c655ca5e987bd878bda7a4287
benjamincrary <benjamin.crary@gmail.com>
2020-08-19 16:38:05 -0500

    Update assign_zonal_representative.R

--- R/assign_zonal_representative.R
+++ R/assign_zonal_representative.R
@@ -8,9 +8,11 @@
 #' @export
 #'
 #' @examples
-assign_zonal_representative <- function(shapefile, zone_field, destination) {
+assign_zonal_representative <- function(shapefile, zone_field, parameter, destination) {
 
   zone_map <- create_zone_map(shapefile, zone_field)
+
+  output_zone_map <- output_zone_map(zone_map, parameter, destination)
 
   identify_zonal_timeseries(zone_map, destination)
 
